bb513cfcdede5e3bca0f1b3c5b61828b49005bdb
tphuc263 <tphuc.ptit@gmail.com>
2025-05-25 18:04:39 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
 
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>ShareApp</h1>
+            <p>Welcome to Photo Sharing App</p>
+        </div>
+    )
+}
+
+export default App
